5f065bfa7ef473653455b537c5390647963652de
andreycoutt <contato.andreyyy@gmail.com>
2023-11-22 20:39:13 -0300

    Tipos de Dados Baseados em Coleções

--- Python Essencial/003 - Tipos de Dados Baseados em Coleções/index.py
+++ Python Essencial/003 - Tipos de Dados Baseados em Coleções/index.py
@@ -13,3 +13,13 @@ print(f"lista_heterogenea: {type(lista_heterogenea)}");
 numeros_sorteados = {3, 5, 5, 11, 28, 55, 57};
 print(numeros_sorteados);
 print(f"numeros_sorteados: {type(numeros_sorteados)}");
+
+
+produtos1 = {1: "banana", 2: "morango", 3: "uva", 4: "abacaxi", 5: "abacate"};
+print(produtos1);
+print(f"produtos1: {type(produtos1)}");
+
+
+pessoa_1 = ("123.456.789-01", "Fulano de tal", True, 1800.00, 46);
+print(pessoa_1);
+print(f"pessoa_1: {type(pessoa_1)}");
