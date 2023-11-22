e02c08fa875da94a282a799a745116d3628bfbdd
andreycoutt <contato.andreyyy@gmail.com>
2023-11-22 19:40:55 -0300

    Tipos de Dados Básicos

--- Python Essencial/Tipos de Dados Básicos/index.py
+++ Python Essencial/Tipos de Dados Básicos/index.py
@@ -8,3 +8,22 @@ print(f"olá, me chamo {nome} e comprei {quantidade} biscoitos a R$ {preco_unita
 print(f"nome: {type(nome)}");
 print(f"quantidade: {type(quantidade)}");
 print(f"preco_unitario: {type(preco_unitario)}");
+
+
+texto_grande = """
+    Este é um texto grande.
+    Este é um texto grande.
+    Este é um texto grande.
+    Este é um texto grande.
+    Este é um texto grande.
+"""
+print(texto_grande);
+
+
+casado = True;
+tem_filhos = False;
+empregado = True;
+
+print(f"casado: {type(casado)}");
+print(f"tem_filhos: {type(tem_filhos)}");
+print(f"empregado: {type(empregado)}");
