f3881156263da188df834980effb458308711f18
andreycoutt <contato.andreyyy@gmail.com>
2024-02-27 21:29:54 -0300

    Operadores Matemáticos e Conversão de Tipos

---
+++ Python Essencial/004 - Operadores Matemáticos e Conversão de Tipos/index.py
@@ -0,0 +1,24 @@
+a = 5; 
+b = 3;
+c = a + b;
+print(c);
+
+
+s1 = "olá";
+s2 = " mundo";
+s3 = s1 + s2;
+print(s3);
+
+d = 5;
+e = 3;
+f = d - e;
+print(f);
+
+g = 5;
+h = 3;
+i = g * h;
+print(i);
+
+s = 'oi';
+r = s * 3;
+print(r);
